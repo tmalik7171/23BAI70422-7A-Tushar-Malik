eee53ea4d73bf72cf828f7751a34bd3b56825c11
tushar7171 <riteshmalik717@gmail.com>
2026-02-05 00:55:12 +0530

    Fix case-sensitive import paths for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AppNavbar from "./components/Navbar";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
+import Home from "./Pages/Home";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 export default function App() {
   return (
